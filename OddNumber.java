6f8993d4d00815114da30307739ab9eb9ee93a56
ashutosh1011 <ramtripathi246@gmail.com>
2024-12-22 16:56:34 +0530

    first commit

---
+++ OddNumber.java
@@ -0,0 +1,14 @@
+public class OddNumber{
+ public static void main(String[] args) {
+    int i=1;
+    int count=0;
+   while (true) { 
+       System.out.println(i);
+       i+=2;
+       count++;
+       if (count==100) {
+        break;
+   }}
+ }
+  
+}
